243683e62668afce2957336a4c7485486561bebd
MariaVeronicaR <63079885+MariaVeronicaR@users.noreply.github.com>
2023-12-06 20:17:41 -0600

    Add delete functionality

--- blog-front/src/components/BlogList.js
+++ blog-front/src/components/BlogList.js
@@ -1,45 +1,53 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Typography } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
-const TABLE_HEAD = ["Title", "Author", "Content", "Publication Date", ""];
+const TABLE_HEAD = ["Title", "Author", "Content", "Publication Date", "", ""];
 
 export function BlogList({ searchTerm }) {
   const [tableRows, setTableRows] = useState([]);
 
   useEffect(() => {
-    // Definir la URL de la API con el término de búsqueda proporcionado
     const apiUrl = `http://localhost:8000/api/v1/blog?title=${searchTerm}&author=${searchTerm}&content=${searchTerm}`;
 
-    // Realizar la solicitud a la API
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
         const sortedData = data.sort((a, b) => b.id - a.id);
-        
-        // Actualizar la variable tableRows con los datos ordenados
         setTableRows(sortedData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, [searchTerm]); // Ejecutar la solicitud cuando cambie el término de búsqueda
- 
+  }, [searchTerm]);
+
+  const handleDelete = (id) => {
+    const apiUrl = `http://localhost:8000/api/v1/blog/${id}`;
+
+    fetch(apiUrl, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Actualizar la lista después de la eliminación exitosa
+          setTableRows((prevRows) => prevRows.filter((row) => row.id !== id));
+        } else {
+          console.error("Error deleting blog:", response.statusText);
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting blog:", error);
+      });
+  };
+
   return (
     <Card className="h-full w-full overflow-scroll mb-3">
       <table className="w-full min-w-max table-auto text-left">
         <thead>
           <tr>
             {TABLE_HEAD.map((head) => (
-              <th
-                key={head}
-                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-              >
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-normal leading-none opacity-70"
-                >
+              <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
                   {head}
                 </Typography>
               </th>
@@ -54,10 +62,10 @@ export function BlogList({ searchTerm }) {
             return (
               <tr key={id}>
                 <td className={classes}>
-                <Link to={`/blogs/${id}`}>
-                  <Typography variant="small" color="blue-gray" className="font-normal underline">
-                    {title}
-                  </Typography>
+                  <Link to={`/blogs/${id}`}>
+                    <Typography variant="small" color="blue-gray" className="font-normal underline">
+                      {title}
+                    </Typography>
                   </Link>
                 </td>
                 <td className={classes}>
@@ -67,7 +75,6 @@ export function BlogList({ searchTerm }) {
                 </td>
                 <td className={classes}>
                   <Typography variant="small" color="blue-gray" className="font-normal">
-                    {/*Solo mostrar los primeros 70 caracteres de contenido*/}
                     {content.slice(0, 70)}{content.length > 70 ? '...' : ''}
                   </Typography>
                 </td>
@@ -77,11 +84,18 @@ export function BlogList({ searchTerm }) {
                   </Typography>
                 </td>
                 <td className={classes}>
-                <Link to={`/blogs/${id}`}>
-                  <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium underline">
-                    Detail
-                  </Typography>
+                  <Link to={`/blogs/${id}`}>
+                    <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium underline">
+                      Detail
+                    </Typography>
                   </Link>
+                </td>
+                <td className={classes}>
+                  <button onClick={() => handleDelete(id)}>
+                  <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium underline">
+                      Delete
+                    </Typography>
+                  </button>
                 </td>
               </tr>
             );
